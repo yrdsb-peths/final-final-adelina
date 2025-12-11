29284075a07c5b692b7bdc47b042c3e576194fd3
person-030 <abigailyang783@gmail.com>
2025-12-11 14:14:31 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -9,7 +9,7 @@ public class MyWorld extends World {
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1200, 800, 1); 
+        super(1000, 600, 1); 
         prepare();
         addObject(player,836,494);
         addObject (playerImage, 0, 0);
