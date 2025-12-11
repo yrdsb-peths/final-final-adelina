deb04e319975ea971c98c00c46903436c844b02b
adelinalyw <adelina.lyw@gmail.com>
2025-12-11 14:24:15 -0500

    added background

--- MyWorld.java
+++ MyWorld.java
@@ -10,6 +10,7 @@ public class MyWorld extends World {
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1); 
+        setBackground ("images/background.PNG");
         prepare();
         addObject(player,836,494);
         addObject (playerImage, 0, 0);
