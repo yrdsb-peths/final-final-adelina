a27e536b3e7f6fab58aa98dccb3a40229f5226f8
adelinalyw <adelina.lyw@gmail.com>
2026-01-05 13:09:55 -0500

    added prepare levels

--- MyWorld.java
+++ MyWorld.java
@@ -168,11 +168,104 @@ public class MyWorld extends World {
     }
     
     private void prepareLevelTwo() {
+        Counter[] counterRow1 = new Counter[5];
+        for (int i=0; i<counterRow1.length; i++) {
+            if (i==0 || i==2){
+                counterRow1[i] = new StoveCounter();
+            } else {
+                counterRow1[i] = new NormalCounter();
+            }
+            
+            addObject(counterRow1[i], 20 + counterOffset + (3+i)*unitWidth, counterOffset + 2*unitWidth);
+        }
+        
+        Counter[] counterRow2 = new Counter[9];
+        for (int i=0; i<counterRow2.length; i++) {
+            if (i==1 || i==3){
+                counterRow2[i] = new CuttingCounter();
+            } else if (i==6){
+                counterRow2[i] = new FoodCounter("onion");
+            }else if (i==7){
+                counterRow2[i] = new FoodCounter("tomato");
+            }else if (i==6){
+                counterRow2[i] = new FoodCounter("mushroom");
+            } else {
+                counterRow2[i] = new NormalCounter();
+            }
+            
+            addObject(counterRow2[i], 20 + counterOffset + (1+i)*unitWidth, counterOffset + 8*unitWidth);
+        }
+        
+        NormalCounter[] counterColumn1 = new NormalCounter[5];
+        for (int i=0; i<counterColumn1.length; i++) {
+            counterColumn1[i] = new NormalCounter();
+            addObject(counterColumn1[i], 20 + counterOffset + unitWidth, counterOffset + (3+i)*unitWidth);
+        }
         
     }
     
     private void prepareLevelThree() {
+        NormalCounter[] counter1 = new NormalCounter[3];
+        for (int i=0; i<3; i++) {
+            counter1[i] = new NormalCounter();
+            addObject(counter1[i],20+(i+1)*unitWidth + counterOffset,2*unitWidth+counterOffset);
+        }
         
+        FoodCounter counter2 = new FoodCounter("onion");
+        addObject(counter2,20+4*unitWidth + counterOffset,2*unitWidth+counterOffset);
+        
+        FoodCounter counter3 = new FoodCounter ("tomato");
+        addObject(counter3,20+5*unitWidth + counterOffset,2*unitWidth+counterOffset);
+        
+        FoodCounter counter4 = new FoodCounter ("mushroom");
+        addObject(counter4,20+6*unitWidth + counterOffset,2*unitWidth+counterOffset);
+        
+        NormalCounter[] counter5 = new NormalCounter[2];
+        for (int i=0; i<2; i++) {
+            counter5[i] = new NormalCounter();
+            addObject(counter5[i],20+(7+i)*unitWidth + counterOffset,2*unitWidth+counterOffset);
+        }
+        
+        StoveCounter counter6 = new StoveCounter();
+        addObject(counter6,20+9*unitWidth + counterOffset,2*unitWidth+counterOffset);
+        
+        StoveCounter counter7 = new StoveCounter();
+        addObject(counter7,20+10*unitWidth + counterOffset,2*unitWidth+counterOffset);
+        
+        NormalCounter[] counter8 = new NormalCounter[3];
+        for (int i=0; i<3; i++) {
+            counter8[i] = new NormalCounter();
+            addObject(counter8[i],20+(11+i)*unitWidth + counterOffset,2*unitWidth+counterOffset);
+        }
+        Plate plate1 = new Plate();
+        addObject(plate1, 20+12*unitWidth + counterOffset, 2*unitWidth+counterOffset);
+        counter8[1].setObjectOnTop (plate1);
+        
+        //delivery counter
+        NormalCounter counter12 = new NormalCounter();
+        addObject (counter12, 20+(13)*unitWidth + counterOffset,3*unitWidth+counterOffset);
+        
+        LeftDeliveryCounter deliveryCounter = new LeftDeliveryCounter();
+        addObject(deliveryCounter, 20+14*unitWidth, 5*unitWidth);
+    
+        addObject (plateCounter, 20+14*unitWidth - counterOffset, 6*unitWidth + counterOffset);
+        
+        // left column of counters
+        NormalCounter[] counter9 = new NormalCounter[3];
+        for (int i=0; i<3; i++) {
+            counter9[i] = new NormalCounter();
+            addObject(counter9[i],20+unitWidth+counterOffset,(i+3) * unitWidth + counterOffset);
+        }
+        
+        // second row of counters
+        NormalCounter[] counter10 = new NormalCounter[9];
+        for (int i=0; i<4; i++) {
+            counter10[i] = new NormalCounter();
+            addObject(counter10[i],20+(i+2)*unitWidth + counterOffset, 5*unitWidth + counterOffset);
+        }
+        
+        CuttingCounter counter11 = new CuttingCounter();
+        addObject(counter11, 20+(6)*unitWidth + counterOffset, 5*unitWidth + counterOffset);
     }
     
     public Onion generateOnion() {
